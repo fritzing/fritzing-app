f92bfbd515c614ad63a730eff62950d11a0be843
irascibl <irascibl@gmail.com>
2013-05-19 06:41:11 +0200

    translation update; new pt_br translation

--- src/help/aboutbox.cpp
+++ src/help/aboutbox.cpp
@@ -166,7 +166,7 @@ QString("<br /><br /><br /><br /><br /><br /><br /><br /><br />") +
     tr("to Lionel Michel and Yvan Kloster for the French translation, " ) +
     tr("to Cihan Mete Bahad&#x0131;r for the Turkish translation, " ) +
    
-	tr("and to Arthur Zanona, Nuno Pessanha Santos, and Leandro Nunes for the Portuguese (Brazilian) translation. " ) +
+	tr("and to Arthur Zanona, Nuno Pessanha Santos, Leandro Nunes, and Gabriel Ferreira for the Portuguese (Brazilian) translation. " ) +
 "</p>" +
 
 "<p>" +

a1acc1b1728fcb78a8325350fc0938986b15d0a9
irascibl <irascibl@gmail.com>
2013-06-01 07:23:57 +0200

    new examples from stefan

--- fritzing/src/program/programwindow.cpp
+++ fritzing/src/program/programwindow.cpp
@@ -174,7 +174,7 @@ void ProgramWindow::initLanguages() {
 		}
 		else {
 			QString name = Syntaxer::parseForName(fileInfo.absoluteFilePath());
-			if (!name.isEmpty() && !name.contains("arduino", Qt::CaseInsensitive)) {		// Arduino is not really available      
+			if (!name.isEmpty() /* && !name.contains("arduino", Qt::CaseInsensitive) */) {		// Arduino is not really available      
 				m_languages.insert(name, fileInfo.absoluteFilePath());
 			}
 		}

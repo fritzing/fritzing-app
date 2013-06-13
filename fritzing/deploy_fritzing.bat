f91ea52f751eef6058eefedd9562f016b553cb54
irascibl <irascibl@gmail.com>
2013-06-13 06:04:55 +0200

    another release script

--- fritzing/deploy_fritzing.bat
+++ fritzing/deploy_fritzing.bat
@@ -1,35 +1,41 @@
 cd /d %~dp0
 
-set svndir="C:\Program Files\SlikSvn\bin\"
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/translations ./release/translations
+mkdir .\release\deploy
 
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/bins ./release/bins
+xcopy .\translations .\release\deploy\translations /E  /I
 
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/sketches ./release/sketches
+xcopy .\bins .\release\deploy\bins /E  /I
 
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/parts ./release/parts
+xcopy .\sketches .\release\deploy\sketches /E  /I
 
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/pdb ./release/pdb
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/help ./release/help
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/README.txt ./release/README.txt
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/LICENSE.GPL2 ./release/LICENSE.GPL2
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/LICENSE.GPL3 ./release/LICENSE.GPL3
-%svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/LICENSE.CC-BY-SA ./release/LICENSE.CC-BY-SA
+xcopy .\parts .\release\deploy\parts /E  /I
+
+xcopy .\pdb .\release\deploy\pdb /E  /I
+xcopy .\help .\release\deploy\help /E  /I
+copy .\README.txt .\release\deploy\README.txt
+copy .\LICENSE.GPL2 .\release\deploy\LICENSE.GPL2
+copy .\LICENSE.GPL3 .\release\deploy\LICENSE.GPL3
+copy .\LICENSE.CC-BY-SA .\release\deploy\LICENSE.CC-BY-SA
+copy .\release\fritzing.exe .\release\deploy\fritzing.exe
+xcopy .\release\*.dll .\release\deploy
 
 
-
-del .\release\translations\*.ts
+del .\release\deploy\translations\*.ts
 cd release
+cd deploy
 cd translations
 for /f "usebackq delims=;" %%A in (`dir /b *.qm`) do If %%~zA LSS 1024 del "%%A"
 cd ..
 cd ..
+cd ..
 
-del .\release\bins\more\sparkfun-*.fzb
-del .\release\pdb\user\*.fzp
-del .\release\parts\svg\user\breadboard\*.svg
-del .\release\parts\svg\user\icon\*.svg
-del .\release\parts\svg\user\pcb\*.svg
-del .\release\parts\svg\user\schematic\*.svg
-del ".\release\parts\svg\user\new schematic\*.svg"
-rmdir ".\release\parts\svg\user\new schematic"
+del .\release\deploy\bins\more\sparkfun-*.fzb
+del .\release\deploy\pdb\user\*.fzp
+del .\release\deploy\parts\svg\user\breadboard\*.svg
+del .\release\deploy\parts\svg\user\icon\*.svg
+del .\release\deploy\parts\svg\user\pcb\*.svg
+del .\release\deploy\parts\svg\user\schematic\*.svg
+del ".\release\deploy\parts\svg\user\new schematic\*.svg"
+rmdir ".\release\deploy\parts\svg\user\new schematic"
+
+echo don't forget about the msvc files and the lib folder

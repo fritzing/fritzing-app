ba19ac5d4f333c2c99b0cd85d932ecfdecc5bc01
irascibl <irascibl@gmail.com>
2013-06-30 06:00:13 +0200

    version update

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -37,10 +37,10 @@ $Date: 2013-05-10 17:47:21 +0200 (Fr, 10. Mai 2013) $
 
 QString Version::m_majorVersion("0");       
 QString Version::m_minorVersion("8");
-QString Version::m_minorSubVersion("0");
+QString Version::m_minorSubVersion("1");
 QString Version::m_modifier("b");
 QString Version::m_svnRevision("$Revision: 08d5 $:");
-QString Version::m_svnDate("$Date: 2013-06-12 07:35:21 +0200 (Mi, 12. Jun 2013) $");
+QString Version::m_svnDate("$Date: 2013-06-30 05:565:21 +0200 (So, 30. Jun 2013) $");
 QString Version::m_revision;
 QString Version::m_date;
 QString Version::m_shortDate;

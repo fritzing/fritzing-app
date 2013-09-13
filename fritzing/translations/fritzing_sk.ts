336fe459c0b67c3a650921350916e25bfc50dfd6
irascibl <irascibl@gmail.com>
2013-09-13 15:21:17 +0200

    translation tweak and update

--- fritzing/translations/fritzing_sk.ts
+++ fritzing/translations/fritzing_sk.ts
@@ -6393,8 +6393,8 @@ Klávesa Alt alebo klávesa %1 = posunúť</translation>
         <location filename="../src/autoroute/mazerouter/mazerouter.cpp" line="665"/>
         <location filename="../src/autoroute/mazerouter/mazerouter.cpp" line="734"/>
         <location filename="../src/autoroute/panelizer.cpp" line="486"/>
-        <location filename="../src/autoroute/panelizer.cpp" line="1367"/>
-        <location filename="../src/autoroute/panelizer.cpp" line="1386"/>
+        <location filename="../src/autoroute/panelizer.cpp" line="1366"/>
+        <location filename="../src/autoroute/panelizer.cpp" line="1385"/>
         <location filename="../src/fapplication.cpp" line="925"/>
         <location filename="../src/mainwindow/fritzingwindow.cpp" line="50"/>
         <location filename="../src/mainwindow/fritzingwindow.cpp" line="143"/>
@@ -7266,12 +7266,12 @@ Fritzing stále funguje, ale nebudete môcť zmeniť vlastnosti súčiastok.</tr
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/autoroute/panelizer.cpp" line="1367"/>
+        <location filename="../src/autoroute/panelizer.cpp" line="1366"/>
         <source>unable to copy file &apos;%1&apos; to &apos;%2&apos;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/autoroute/panelizer.cpp" line="1385"/>
+        <location filename="../src/autoroute/panelizer.cpp" line="1384"/>
         <source>%2 ... %1 wires moved from their saved position</source>
         <translation type="unfinished"></translation>
     </message>
@@ -8353,7 +8353,7 @@ Note: you can also trigger this display by mousing down on the routing status te
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/items/tracewire.cpp" line="84"/>
+        <location filename="../src/items/tracewire.cpp" line="90"/>
         <source>width</source>
         <translation>šírka</translation>
     </message>
@@ -8685,12 +8685,12 @@ Note: you can also trigger this display by mousing down on the routing status te
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/items/wire.cpp" line="1542"/>
+        <location filename="../src/items/wire.cpp" line="1543"/>
         <source>color</source>
         <translation>farba</translation>
     </message>
     <message>
-        <location filename="../src/items/wire.cpp" line="1566"/>
+        <location filename="../src/items/wire.cpp" line="1567"/>
         <source>Banded</source>
         <translation type="unfinished"></translation>
     </message>

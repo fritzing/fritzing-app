1a99477457b257886a01c09f8f22ab5e6c77c90c
irascibl <irascibl@gmail.com>
2013-09-20 21:30:21 +0200

    new schematic--still in progress

--- fritzing/tools/brd2svg/brdapplication.h
+++ fritzing/tools/brd2svg/brdapplication.h
@@ -90,6 +90,8 @@ protected:
 	bool matchAnd(QDomElement & contact, QDomElement & connector);
     QString findSubpart(const QString & name, QHash<QString, QString> & subpartAliases, QDir & subpartsFolder);
     bool registerFonts();
+    QString schematicPinNumber(qreal x1, qreal x2, qreal y, qreal pinSmallTextHeight, qreal pinSmallTextVert, const QString & id, bool rotate);
+    QString schematicPinText(const QString & id, const QString & signal, qreal x, qreal y, qreal bigPinFontSize, const QString & anchor, bool rotate);
 
 protected:
     QString m_workingPath;

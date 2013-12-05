08e2cdb9a69a3bdc732b24ff98a3090c5589dbea
irascibl <irascibl@gmail.com>
2013-12-05 14:44:17 +0100

    welcome view text and url tweak

--- src/sketch/welcomeview.cpp
+++ src/sketch/welcomeview.cpp
@@ -461,8 +461,8 @@ QWidget * WelcomeView::initShop() {
 
     m_shopUberFrame = createShopContentFrame(":/resources/images/welcome_kit.png", 
                                                 tr("Fritzing CreatorKit"), 
-                                                tr("Get a Fritzing Creator Kit and invent your own electronics. It's as easy as bicycling."),
-                                                "http://fritzing.org/creatorkit/",
+                                                tr("The Fritzing Creator Kit provides a fast, easy and entertaining introduction to the world of interactive electronics."),
+                                                "http://creatorkit.fritzing.org/",
                                                 tr("order now >>"),
                                                 tr("Get your Creator Kit now."),
                                                 ":/resources/images/icons/WS-shopLogo.png"
@@ -471,7 +471,7 @@ QWidget * WelcomeView::initShop() {
 
     m_fabUberFrame = createShopContentFrame(":/resources/images/pcbs_2013.png", 
                                                 tr("Fritzing FAB"), 
-                                                tr("Fritzing FAB easily brings you high quality hardware of your own design."),
+                                                tr("Fritzing FAB is an easy and affordable service for producing professional PCBs from your Fritzing sketches."),
                                                 "http://fab.fritzing.org/",
                                                 tr("produce your first pcb now >>"),
                                                 tr("Order your PCB now."),

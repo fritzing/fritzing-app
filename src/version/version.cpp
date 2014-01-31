916aae3528c3763aeaa228d2d1996c968e11a551
irascibl <irascibl@gmail.com>
2014-01-31 06:22:33 +0100

    version increment

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -37,10 +37,10 @@ $Date: 2013-05-10 17:47:21 +0200 (Fr, 10. Mai 2013) $
 
 QString Version::m_majorVersion("0");       
 QString Version::m_minorVersion("8");
-QString Version::m_minorSubVersion("7");
+QString Version::m_minorSubVersion("8");
 QString Version::m_modifier("b");
-QString Version::m_svnRevision("$Revision: 1bf5 $:");
-QString Version::m_svnDate("$Date: 2014-01-24 17:10:32 +0200 (Fr, 24 Jan 2014) $");
+QString Version::m_svnRevision("$Revision: da4e $:");
+QString Version::m_svnDate("$Date: 2014-01-31 06:19:32 +0200 (Fr, 31 Jan 2014) $");
 QString Version::m_revision;
 QString Version::m_date;
 QString Version::m_shortDate;

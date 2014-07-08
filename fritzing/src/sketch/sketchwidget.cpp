20611d958e0b98a40f7853da282287f75fcac819
bluearc.in.sky@gmail.com <bluearc.in.sky@gmail.com>
2014-07-08 09:55:53 +0200

    ratsnest fix for qt5.3.1

--- fritzing/src/sketch/sketchwidget.cpp
+++ fritzing/src/sketch/sketchwidget.cpp
@@ -991,12 +991,13 @@ ItemBase * SketchWidget::findItem(long id) {
 				if (lk->id() == id) return lk;
 			}
 
-			if (chief->layerKin().count() == 0) {
-				// going from layerkin in one view to non-layered part in another view
-				return chief;
-			}
+        //    if (chief->layerKin().count() == 0) {
+        //         going from layerkin in one view to non-layered part in another view
+        //        return chief;
+        //   }
+        // return NULL;
 
-			return NULL;
+           return chief;
 		}
 	}
 

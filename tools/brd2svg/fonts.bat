8b2d55b653948eafb24db62066b5ea1d54171cea
André Knörig <andre.knoerig@ixds.de>
2014-09-25 15:53:35 +0200

    moved brd2svg into its own repo eagle2fritzing

--- tools/brd2svg/fonts.bat
+++
@@ -1,2 +0,0 @@
-cd %~dp0
-C:\Qt\qt-everywhere-opensource-src-4.8.3\bin\rcc.exe -binary brd2svgResources.qrc -o and/fonts.rcc
--- tools/brd2svg/make.bat
+++
@@ -1,2 +0,0 @@
-cd %~dp0
-C:\Qt\qt-everywhere-opensource-src-4.8.3\bin\qmake.exe -t vcapp -spec win32-msvc2010 brd2svg.pro 

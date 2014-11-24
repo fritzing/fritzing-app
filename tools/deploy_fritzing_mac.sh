9887b5bdc11f844230b83c853c059ede7b0b348a
André Knörig <andre.knoerig@ixds.de>
2014-11-24 23:02:08 +0100

    update mac deploy script to new repository structure

--- tools/deploy_fritzing_mac.sh
+++ tools/deploy_fritzing_mac.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-echo please check phoenix.pro to make sure that only x86_64 exists in the line:
-echo "    CONFIG += x86_64 #x86 ppc"
-
-QTBIN=/Users/jonathancohen/Qt/5.2.1/clang_64/bin
+QTBIN=/Users/andre/Qt5.2.1/5.2.1/clang_64/bin
 
 tdir=`dirname $BASH_SOURCE`
 cd $tdir
@@ -15,10 +11,10 @@ echo $currentdir
 
 echo building fritzing
 $QTBIN/qmake -o Makefile phoenix.pro
-make release  # release is the type of build, not the 
-releasedir=$currentdir/../../release64
+make release  # release is the type of build
+releasedir=$currentdir/../release64
 
-deploydir=$currentdir/../../deploy
+deploydir=$currentdir/../deploy-src
 echo "deploy directory"
 echo $deploydir
 echo $releasedir
@@ -26,9 +22,9 @@ echo $releasedir
 rm -rf $deploydir
 mkdir $deploydir
 
-git clone https://code.google.com/p/fritzing/ $deploydir
+git clone --recursive https://github.com/fritzing/fritzing-app/ $deploydir
 
-fritzingdir=$deploydir/fritzing
+fritzingdir=$currentdir/../deploy-app
 echo "fritzing directory"
 echo $fritzingdir
 
@@ -39,24 +35,17 @@ find $fritzingdir/translations -name "*.qm" -size -128c -delete
 
 echo "still more cleaning"
 rm $fritzingdir/control
-rm -rf $fritzingdir/datasheets/*
-rmdir $fritzingdir/datasheets
 rm -rf $fritzingdir/deploy*
 rm -rf $fritzingdir/fritzing.*
 rm $fritzingdir/Fritzing.1
 rm $fritzingdir/Fritzing.sh
 rm -rf $fritzingdir/Fritzing*.plist
 rm -rf $fritzingdir/linguist*
-rm -rf $fritzingdir/part-gen-scripts/*
-rmdir $fritzingdir/part-gen-scripts
-rm -rf $fritzingdir/not_quite_ready/*
-rmdir $fritzingdir/not_quite_ready
 rm -rf $fritzingdir/phoenix*
 rm -rf $fritzingdir/pri/*
 rmdir $fritzingdir/pri
 rm -rf $fritzingdir/resources/*
 rmdir $fritzingdir/resources
-rm $fritzingdir/Setup*
 rm -rf $fritzingdir/src/*
 rmdir $fritzingdir/src
 rm -rf $fritzingdir/tools/*
@@ -64,11 +53,5 @@ rmdir $fritzingdir/tools
 
 echo mac deploy qt
 cp -r $releasedir/Fritzing.app $deploydir
-$QTBIN/macdeployqt $deploydir/Fritzing.app 
+$QTBIN/macdeployqt $deploydir/Fritzing.app
 cp -r $fritzingdir/* $deploydir/Fritzing.app/Contents/MacOS
-
-
-#/Developer/Tools/Qt/macdeployqt /Users/jonathancohen/fritzing/fritzing/deploy/fritzing/Fritzing.app 
-
-# may need this symlink to get carbon to build
-#sudo ln -s /Developer/Platforms/iPhoneOS.platform/Developer/usr/libexec/gcc/darwin/ppc /usr/libexec/gcc/darwin/ppc

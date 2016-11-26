a1a98f042f09938c21ce3bc5e29bee55b7763668
GQDeltex <operitum@gmail.com>
2016-11-26 13:25:17 +0100

    Changing Linux Installer

--- install_fritzing.sh
+++ install_fritzing.sh
@@ -44,3 +44,34 @@ update-mime-database "${MIMEDIR}"
 xdg-icon-resource forceupdate --mode user
 
 echo "installed fritzing system icons"
+
+#Defining new Install Path
+install_path=~/.fritzing-0.9.3b/
+
+#Check if Installation exists
+if [ ! -f $install_path ]
+then
+	echo "Installing"
+	mkdir $install_path
+	cp -r ./* $install_path
+	echo "Copied all files"
+else
+	echo "Already Installed"
+fi
+
+#Put fritzing.desktop to right place and place file for command line startup in /bin/
+sudo cp ./fritzing.desktop /usr/share/applications/fritzing.desktop
+sudo mv  ./fritzing /bin/fritzing
+sudo cp ./icons/fritzing_icon.png /usr/share/icons/fritzing_icon.png
+#Making it all executable
+sudo chmod a+x /bin/fritzing
+sudo chmod a+x /usr/share/applications/fritzing.desktop
+echo "Set up Starter icons"
+
+#Removing unneccesary Directory
+working_dir=${PWD##*/}
+echo "Removing $working_dir"
+cd ..
+sudo rm -r ./$working_dir
+echo "Cleaned"
+echo "Fritzing is now located at $install_path"

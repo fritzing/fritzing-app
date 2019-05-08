f1be4101e151931bd1b459ac3b41e289c79b1515
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2019-05-09 00:11:29 +0300

    fix and test VsDevCmd call

--- tools/build-windows.bat
+++ tools/build-windows.bat
@@ -1,5 +1,6 @@
 REM echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64 || exit /b 1
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test || exit /b 1
 
 tools\release_fritzing.bat  0.9.4b 64 2017

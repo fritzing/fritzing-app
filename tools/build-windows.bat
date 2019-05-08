04f03ed9ecf78495eb17358a0767aa903a813dc9
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2019-05-09 00:35:26 +0300

    call VsDevCmd scripts

--- tools/build-windows.bat
+++ tools/build-windows.bat
@@ -1,6 +1,6 @@
 REM echo off
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 
 tools\release_fritzing.bat  0.9.4b 64 2017

86319414fc8df1d381f05e2f2ea21ace96cc3730
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2019-05-08 23:33:33 +0300

    MS CLI for x64

--- tools/build-windows.bat
+++ tools/build-windows.bat
@@ -1,5 +1,5 @@
 REM echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64
 
 tools\release_fritzing.bat  0.9.4b 64 2017

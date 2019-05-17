fe9e72abaaa24d2dcd2517a4552f8f19db69a1cd
Chris Mayo <aklhfex@gmail.com>
2019-05-17 19:22:07 +0100

    Support system QuaZIP on Qt 5
    QuaZIP 0.7.2 added dual Qt 4/5 support by installing for Qt 5:
        /usr/include/quazip5/quazip.h
        /usr/include/quazip5/quazipfile.h
        /usr/lib64/libquazip5.so
    
    https://github.com/stachenov/quazip/commit/cc2e8ff9bc45679c3312592cce39ccaf62c66a00

--- src/utils/folderutils.cpp
+++ src/utils/folderutils.cpp
@@ -35,8 +35,8 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../debugdialog.h"
 #ifdef QUAZIP_INSTALLED
-#include <quazip/quazip.h>
-#include <quazip/quazipfile.h>
+#include <quazip5/quazip.h>
+#include <quazip5/quazipfile.h>
 #else
 #include "../lib/quazip/quazip.h"
 #include "../lib/quazip/quazipfile.h"

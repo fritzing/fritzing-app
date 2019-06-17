a42e26b8c8a077f728b467cdac3746906a1d325f
Kjell Morgenstern <kjell@gmx.de>
2019-06-17 22:29:57 +0200

    Add MacOS CD build to release

--- tools/aws_tasks.sh
+++ tools/aws_tasks.sh
@@ -4,29 +4,63 @@
 # It enables travis jobs to store builds on S3 for release packaging
 
 function setup {
-    mkdir -p ~/.aws
-
-    cat > ~/.aws/credentials << EOL
-[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOL
-
-    # set up awscli packages
-    pip install --user awscli
-    mkdir -p ~/"$TRAVIS_BUILD_NUMBER"
-    aws s3 sync s3://fritzing/"$TRAVIS_BUILD_NUMBER" ~/"$TRAVIS_BUILD_NUMBER"
+    x_aws_credentials
+    x_aws_install
 }
 
-function synchronize {
-    aws s3 sync ~/"$TRAVIS_BUILD_NUMBER" s3://fritzing/"$TRAVIS_BUILD_NUMBER"
+function synchronize_down {
+    aws s3 sync s3://fritzing/"$TRAVIS_BUILD_NUMBER" "$HOME/$TRAVIS_BUILD_NUMBER"
+}
+
+function synchronize_up {
+    aws s3 sync "$HOME/$TRAVIS_BUILD_NUMBER" s3://fritzing/"$TRAVIS_BUILD_NUMBER"
 }
 
 function cleanup {
     aws s3 rm --recursive s3://fritzing/"$TRAVIS_BUILD_NUMBER" # clean up after ourselves
 }
 
+function x_aws_install {
+    case "$TRAVIS_OS_NAME" in
+        linux*)
+            pip install --user awscli
+            ;;
+        osx*)
+            pip3 install --user awscli
+            ;;
+        windows*)
+            choco install awscli
+            # type "C:/ProgramData/chocolatey/logs/chocolatey.log"
+            ;;
+    esac
+}
+
+function x_aws_env {
+    case "$TRAVIS_OS_NAME" in
+        linux*)
+            export PATH=$HOME/.local/bin:$PATH
+            ;;
+        osx*)
+            export PATH=$HOME/Library/Python/3.7/bin:$PATH
+            ;;
+        windows*)
+            export PATH="$PATH:$USERPROFILE/AppData/Local/Programs/Python/Python37/Scripts:C:/Program Files (x86)/Amazon/AWSCLI\bin"
+            ;;
+    esac
+}
+
+function x_aws_credentials {
+    mkdir -p "$HOME"/.aws
+    cat > "$HOME"/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+}
+
 if [[ ( "$TRAVIS_PULL_REQUEST" == false ) && ( "$TRAVIS_BRANCH" == "develop" ) && ( "$TRAVIS_REPO_SLUG" == "KjellMorgenstern/fritzing-app" )]]; then
-    echo "Running deploy task '$1'"
+    x_aws_env
+  
+    echo "Running deploy task '$1' on $TRAVIS_OS_NAME ( $OSTYPE )"
     $1;
 fi
--- tools/build-windows.bat
+++ tools/build-windows.bat
@@ -3,4 +3,4 @@ REM echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 
-tools\release_fritzing.bat  0.9.4b 64 2017
+tools\release_fritzing.bat %1 64 2017

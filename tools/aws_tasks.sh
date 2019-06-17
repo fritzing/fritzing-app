dc3aa51987a7811f1fc914be8c523c2baa87f8d8
Kjell Morgenstern <kjell@gmx.de>
2019-06-17 14:08:18 +0200

    Refactor to aws_tasks

--- tools/aws_tasks.sh
+++ tools/aws_tasks.sh
@@ -3,15 +3,29 @@
 # This script is used on main repo via travis.
 # It enables travis jobs to store builds on S3 for release packaging
 
-mkdir -p ~/.aws
+function setup {
+    mkdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
+    cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-# set up awscli packages
-pip install --user awscli
-mkdir -p ~/$TRAVIS_BUILD_NUMBER
-aws s3 sync s3://fritzing/$TRAVIS_BUILD_NUMBER ~/$TRAVIS_BUILD_NUMBER
+    # set up awscli packages
+    pip install --user awscli
+    mkdir -p ~/"$TRAVIS_BUILD_NUMBER"
+    aws s3 sync s3://fritzing/"$TRAVIS_BUILD_NUMBER" ~/"$TRAVIS_BUILD_NUMBER"
+}
+
+function synchronize {
+    aws s3 sync ~/"$TRAVIS_BUILD_NUMBER" s3://fritzing/"$TRAVIS_BUILD_NUMBER";
+}
+
+function cleanup {
+    aws s3 rm --recursive s3://fritzing/"$TRAVIS_BUILD_NUMBER" # clean up after ourselves
+}
+
+if [[ ( "$TRAVIS_PULL_REQUEST" == false ) && ( "$TRAVIS_BRANCH" = "develop" ) ]]; then
+    $1;
+fi

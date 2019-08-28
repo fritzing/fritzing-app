32ab1e863c633bbd6bd926dfcc51f14b9b9d50e6
Kjell Morgenstern <kjell@gmx.de>
2019-08-28 13:34:20 +0200

    Run build in docker --privileged mode

--- docker/release-linux.sh
+++ docker/release-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing fritzing/build:"$1" docker/xvfb-release-helper.sh "$2"
+docker run --privileged -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing fritzing/build:"$1" docker/xvfb-release-helper.sh "$2"

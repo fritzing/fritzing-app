1121e0780cf53a802a5763e25e2ee227ef301b64
Kjell Morgenstern <kjell@gmx.de>
2019-10-07 21:17:44 +0200

    Shell linting

--- docker/release-linux.sh
+++ docker/release-linux.sh
@@ -2,6 +2,6 @@
 set -xe
 
 docker run --privileged -v "$(pwd):/home/conan/fritzing" \
-    -e TRAVIS=${TRAVIS:-} \
-    -e TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-} \
+    -e TRAVIS="${TRAVIS:-}" \
+    -e TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER:-}" \
     -w /home/conan/fritzing fritzing/build:"$1" docker/xvfb-release-helper.sh "$2"

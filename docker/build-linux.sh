0607266e840ee7047531c22b052388d353e9fa18
cmbahadir <c.mete.bahadir@gmail.com>
2019-10-12 20:25:42 +0300

    Added missing libraries for build.

--- docker/build-linux.sh
+++ docker/build-linux.sh
@@ -2,5 +2,6 @@
 set -xe
 
 mkdir build
+sudo apt-get install libssh2-1-dev libcurl4-openssl-dev libhttp-parser*
 docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic qmake ../phoenix.pro
 docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic make -j16

a8c537765dbd5581e723666627914e1708a89626
cmbahadir <c.mete.bahadir@gmail.com>
2019-10-12 21:14:37 +0300

    Added sudo.

--- docker/build-linux.sh
+++ docker/build-linux.sh
@@ -2,6 +2,6 @@
 set -xe
 
 mkdir build
-apt-get install libssh2-1-dev libcurl4-openssl-dev libhttp-parser-dev
+sudo apt-get install libssh2-1-dev libcurl4-openssl-dev libhttp-parser-dev
 docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic qmake ../phoenix.pro
 docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic make -j16

49b8f8a6e56d37e5dc7a73607f0d599088cd09d3
Joshua Scoggins <Theoretically.x64@gmail.com>
2020-01-03 17:16:09 -0800

    Only fix the svg in loadAux if we can initially load it

--- src/fsvgrenderer.cpp
+++ src/fsvgrenderer.cpp
@@ -117,14 +117,6 @@ QByteArray FSvgRenderer::loadSvg(const QByteArray & contents, const LoadInfo & l
 QByteArray FSvgRenderer::loadAux(const QByteArray & theContents, const LoadInfo & loadInfo)
 {
 	QByteArray cleanContents(theContents);
-	bool cleaned = false;
-
-	QString string(cleanContents);
-    cleaned = TextUtils::fixMuchAndPixelDimensionsIn(string, false);
-	if (cleaned) {
-		cleanContents = string.toUtf8();
-	}
-
 	if (loadInfo.connectorIDs.count() > 0 || !loadInfo.setColor.isEmpty() || loadInfo.findNonConnectors) {
 		QString errorStr;
 		int errorLine;
@@ -133,6 +125,9 @@ QByteArray FSvgRenderer::loadAux(const QByteArray & theContents, const LoadInfo 
 		if (!doc.setContent(cleanContents, &errorStr, &errorLine, &errorColumn)) {
 			DebugDialog::debug(QString("renderer loadAux failed %1 %2 %3 %4").arg(loadInfo.filename).arg(errorStr).arg(errorLine).arg(errorColumn));
 		}
+
+        QString string(cleanContents);
+        TextUtils::fixMuchAndPixelDimensionsIn(string, doc, false);
 
 		bool resetContents = false;
 
@@ -158,22 +153,6 @@ QByteArray FSvgRenderer::loadAux(const QByteArray & theContents, const LoadInfo 
 			cleanContents = TextUtils::removeXMLEntities(doc.toString()).toUtf8();
 		}
 	}
-
-
-	/*
-	cleanContents = doc.toByteArray();
-
-	//QFile file("all.txt");
-	//if (file.open(QIODevice::Append)) {
-		//QTextStream t(&file);
-		//t << cleanContents;
-		//file.close();
-	//}
-
-	*/
-
-	//DebugDialog::debug(cleanContents.data());
-
 	return finalLoad(cleanContents, loadInfo.filename);
 }
 

243dd6a6c81ff1ec62d6bc8fbc8cb1370d796aaa
H. Phil Duby <philduby@phriendly.net>
2020-04-27 15:33:01 -0600

    fix svg path element parse and matching of extra graphics to pcb connector pad

--- tests/auto/test_svg/test_pathlexer_public.cpp
+++ tests/auto/test_svg/test_pathlexer_public.cpp
@@ -2,7 +2,7 @@
 #include "svg/svgpathlexer.h"
 
 /*
-Test how SVGPathLexer::lexer processes various svg path element data strings
+Test how SVGPathLexer::lexer processes various svg path element data strings
 */
 
 #include <boost/test/unit_test.hpp>
@@ -29,12 +29,12 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 		"mmmm" // m0,0
 			"aaaaaaaaaaaaaa" // a 2.6,2.6 0 0 1 5.2,0
 			"vv" // v5.2
-			"aaaaaaaaaaaaa" // a 2.6,2.6 0 0 1-5.2,0
+			"aaaaaaaaaaaaaa" // a 2.6,2.6 0 0 1 -5.2,0
 			"z" // z
 			"mmmm" // m 0.5,3
 			"aaaaaaaaaaaaaa" // a 1,  1   0 0 0 4.2,0
 			"vv" // v-0.8
-			"aaaaaaaaaaaaa" // a 1,  1   0 0 0-4.2,0
+			"aaaaaaaaaaaaaa" // a 1,  1   0 0 0 -4.2,0
 			"zz", // z  "
 		"mmmm" // m0,0
 			"aaaaaaaaaaaaaa" // a 2.6,2.6 0 0 1 5.2,0
@@ -44,7 +44,7 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 			"mmmm" // m 0.5,3
 			"aaaaaaaaaaaaaa" // a 1,  1   0 0 0 4.2,0
 			"vv" // v-0.8
-			"aaaaaaaaaaaaa" // a 1,  1   0 0 0-4.2,0
+			"aaaaaaaaaaaaaa" // a 1,  1   0 0 0 -4.2,0
 			"zz", // z  "
 	};
 	const QList<QList<double>> expectedNumbers = {
@@ -56,12 +56,12 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 		{ 0.0, 0.0, 0.0, 0.0, // m0,0
 			0.0, 2.6, 2.6, 2.6, 2.6, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 5.2, 5.2, 0.0, // a 2.6,2.6 0 0 1 5.2,0
 			0.0, 5.2, // v5.2
-			5.2, 2.6, 2.6, 2.6, 2.6, 0.0, 0.0, 0.0, 0.0, 1.0, -5.2, -5.2, 0.0, // a 2.6,2.6 0 0 1-5.2,0
+			5.2, 2.6, 2.6, 2.6, 2.6, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, -5.2, -5.2, 0.0, // a 2.6,2.6 0 0 1-5.2,0
 			0.0, // z
 			0.0, 0.5, 0.5, 3.0, // m 0.5,3
 			3.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.2, 4.2, 0.0, // a 1,  1   0 0 0 4.2,0
 			0.0, -0.8, // v-0.8
-			-0.8, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -4.2, -4.2, 0.0, // a 1,  1   0 0 0-4.2,0
+			-0.8, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -4.2, -4.2, 0.0, // a 1,  1   0 0 0-4.2,0
 			0.0, 0.0 }, // "z  "
 		{ 0.0, 0.0, 0.0, 0.0, // m0,0
 			0.0, 2.6, 2.6, 2.6, 2.6, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 5.2, 5.2, 0.0, // a 2.6,2.6 0 0 1 5.2,0
@@ -71,7 +71,7 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 			0.0, 0.5, 0.5, 3.0, // m 0.5,3
 			3.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.2, 4.2, 0.0, // a 1,  1   0 0 0 4.2,0
 			0.0, -0.8, // v-0.8
-			-0.8, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -4.2, -4.2, 0.0, // a 1,  1   0 0 0-4.2,0
+			-0.8, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -4.2, -4.2, 0.0, // a 1,  1   0 0 0-4.2,0
 			0.0, 0.0 }, // z
 	};
 	const QList<QList<int>> expectedResults = {
@@ -102,25 +102,25 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 25 */
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 27 */
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 29 */
-			SVGPathGrammar::NUMBER, /* 30 */
-			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 32 */
-		  SVGPathGrammar::ZEE, /* 33 */
-		  SVGPathGrammar::EM, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 37 */
-		  SVGPathGrammar::AE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 41 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 43 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 45 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 47 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 49 */
-			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 51 */
-		  SVGPathGrammar::VEE, SVGPathGrammar::NUMBER, /* 53 */
-		  SVGPathGrammar::AE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 57 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 59 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 61 */
-			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 63 */
-			SVGPathGrammar::NUMBER, /* 64 */
-			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 66 */
-		  SVGPathGrammar::ZEE, /* 67 */
-		  SVGPathGrammar::EOF_SYMBOL /* 68 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 31 */
+			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 33 */
+		  SVGPathGrammar::ZEE, /* 34 */
+		  SVGPathGrammar::EM, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 38 */
+		  SVGPathGrammar::AE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 42 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 44 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 46 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 48 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 50 */
+			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 52 */
+		  SVGPathGrammar::VEE, SVGPathGrammar::NUMBER, /* 54 */
+		  SVGPathGrammar::AE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 58 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 60 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 62 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 64 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 66 */
+			SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 68 */
+		  SVGPathGrammar::ZEE, /* 69 */
+		  SVGPathGrammar::EOF_SYMBOL /* 70 */
 		},
 		{ SVGPathGrammar::EM, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 3 */
 		  SVGPathGrammar::AE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 7 */
@@ -149,9 +149,9 @@ BOOST_AUTO_TEST_CASE( pathlexer_tree )
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 60 */
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 62 */
 			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, /* 64 */
-			SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 67 */
-		  SVGPathGrammar::ZEE, /* 68 */
-		  SVGPathGrammar::EOF_SYMBOL /* 69 */
+			SVGPathGrammar::WHITESPACE, SVGPathGrammar::NUMBER, SVGPathGrammar::COMMA, SVGPathGrammar::NUMBER, /* 68 */
+		  SVGPathGrammar::ZEE, /* 69 */
+		  SVGPathGrammar::EOF_SYMBOL /* 70 */
 		}
 	};
 

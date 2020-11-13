514b0c7ca43fb6099b2897774fafc2fab6f8a98d
afaina <anfv@itu.dk>
2020-11-13 16:04:59 +0100

    remove the symbols from the property values

--- src/mainwindow/mainwindow_export.cpp
+++ src/mainwindow/mainwindow_export.cpp
@@ -67,6 +67,7 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 #include "../utils/autoclosemessagebox.h"
 #include "../svg/gerbergenerator.h"
 #include "../processeventblocker.h"
+#include "../items/propertydef.h"
 
 static QString eagleActionType = ".eagle";
 static QString gerberActionType = ".gerber";
@@ -1447,6 +1448,17 @@ QString MainWindow::getSpiceNetlist(QString simulationName, QList< QList<class C
 				}
 			}
 			else {
+				//Find the symbol of this property
+				QString symbol;
+				QHash<PropertyDef *, QString> propertyDefs;
+				PropertyDefMaster::initPropertyDefs(itemBase->modelPart(), propertyDefs);
+				foreach (PropertyDef * propertyDef, propertyDefs.keys()) {
+					if (token.compare(propertyDef->name, Qt::CaseInsensitive) == 0) {
+						symbol = propertyDef->symbol;
+						break;
+					}
+				}
+				//Find the value of the property
 				QVariant variant = itemBase->modelPart()->localProp(token);
 				if (variant.isNull()) {
 					replacement = itemBase->modelPart()->properties().value(token, "");
@@ -1458,6 +1470,10 @@ QString MainWindow::getSpiceNetlist(QString simulationName, QList< QList<class C
 				}
 				else {
 					replacement = variant.toString();
+				}
+				//Remove the symbol, if any
+				if (!symbol.isEmpty()) {
+					replacement.replace(symbol, "");
 				}
 			}
 

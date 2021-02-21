a68099544af62a83928ebcd8889220601b7361c6
afaina <anfv@itu.dk>
2021-02-21 21:34:58 +0100

    The background color of the combo boxes is changed in the capacitor class by calling the bounded reg exp validator, instead of changing the color directly in the bounded reg ex validator.

--- src/items/capacitor.h
+++ src/items/capacitor.h
@@ -39,6 +39,7 @@ public:
 	void getProperties(QHash<QString, QString> &);
 
 public slots:
+	void textModified(const QString & text);
 	void propertyEntry(const QString & text);
 	void simplePropertyEntry(const QString & text);
 

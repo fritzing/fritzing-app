12c4ad8512b455b148c239e9a17e8a756312bf01
Josh Morgenstern <josh@fritzing.org>
2021-07-24 11:18:15 +0000

    Translated using Weblate (Russian)
    Currently translated at 99.7% (1819 of 1823 strings)
    
    Translation: Fritzing/fritzing-app

--- translations/fritzing_ru.ts
+++ translations/fritzing_ru.ts
@@ -4919,7 +4919,7 @@ Reason: %2 (errcode %3)</source>
     <message>
         <location filename="../src/sketch/pcbsketchwidget.cpp" line="2218"/>
         <source>Your sketch does not have a board yet!  Please add a PCB to use this selection operation.</source>
-        <translation type="unfinished"></translation>
+        <translation>У вашего эскиза ещё нет платы! Пожалуйста, добавьте печатную плату, чтобы использовать эту операцию выбора.</translation>
     </message>
     <message>
         <location filename="../src/sketch/pcbsketchwidget.cpp" line="2246"/>

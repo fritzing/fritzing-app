af62546b986f4491c3da6ad0c7ac91e0492c070b
Kjell Morgenstern <kjell@gmx.de>
2021-08-12 16:15:01 +0200

    Fixed warnings about initialization order.

--- src/partseditor/pemainwindow.cpp
+++ src/partseditor/pemainwindow.cpp
@@ -290,16 +290,16 @@ void IconSketchWidget::addViewLayers() {
 
 PEMainWindow::PEMainWindow(ReferenceModel * referenceModel, QWidget * parent)
 	: MainWindow(referenceModel, parent),
-	m_useNextPick(false),
-	m_inPickMode(false),
-	m_gaveSaveWarning(false),
-	m_canSave(false),
+	m_connectorsView(nullptr),
+	m_peToolView(nullptr),
+	m_peSvgView(nullptr),
 	m_guid(TextUtils::getRandText()),
 	m_prefix("prefix0000"),
 	m_fileIndex(0),
-	m_peToolView(nullptr),
-	m_peSvgView(nullptr),
-	m_connectorsView(nullptr)
+	m_canSave(false),
+	m_gaveSaveWarning(false),
+	m_inPickMode(false),
+	m_useNextPick(false)
 {
     m_settingsPrefix = "pe/";
 	m_viewThings.insert(ViewLayer::BreadboardView, new ViewThing);

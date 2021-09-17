dccff5d1b8ab73f65dde0b682712c0ec07413374
Kjell Morgenstern <kjell@gmx.de>
2021-09-17 11:44:11 +0200

    Fixed warning about unused parameters in program/platformlaunchpad.cpp.

--- src/program/platformlaunchpad.cpp
+++ src/program/platformlaunchpad.cpp
@@ -10,7 +10,7 @@ PlatformLaunchpad::PlatformLaunchpad() : Platform(QString("Launchpad"))
 	setExtensions(QStringList() << ".txt");
 }
 
-void PlatformLaunchpad::upload(const QString &port, const QString &board, const QString &fileLocation, QTextEdit *console)
+void PlatformLaunchpad::upload(const QString &, const QString &, const QString &, QTextEdit *)
 {
 	// see http://www.ti.com/tool/msp430-flasher
 	// http://processors.wiki.ti.com/index.php/MSP430_Flasher_-_Command_Line_Programmer

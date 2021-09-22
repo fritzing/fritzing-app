fritzing/fritzing-app

commit 1f0953f4f5a6137a667ee5a1e5b87a916f925e5b

fixes nett labels, issues #3340 and  #2308.

src/items/symbolpaletteitem.cpp
@@ -144,7 +144,7 @@ SymbolPaletteItem::~SymbolPaletteItem() {
 void SymbolPaletteItem::removeMeFromBus(double v) {
 	foreach (ConnectorItem * connectorItem, cachedConnectorItems()) {
 		if (m_isNetLabel) {
-			LocalNetLabels.remove(getLabel(), connectorItem);
+			LocalNetLabels.remove(m_label, connectorItem);
 		}
 		else {
 			double nv = useVoltage(connectorItem);
@@ -243,10 +243,11 @@ void SymbolPaletteItem::setProp(const QString & prop, const QString & value) {
 }
 
 void SymbolPaletteItem::setLabel(const QString & label) {
-	removeMeFromBus(0);
+	removeMeFromBus(0); //Remove this specific item (bb, sch or pcb) from the previous net label
+	m_label = label; // Update the label for this specific item (bb, sch or pcb)
+	m_modelPart->setLocalProp("label", label); //This line modifies the property label in the bb, sch and pcb items
 
-	m_modelPart->setLocalProp("label", label);
-
+	//Add the conectors of the item to the new net label
 	foreach (ConnectorItem * connectorItem, cachedConnectorItems()) {
 		LocalNetLabels.insert(label, connectorItem);
 	}
@@ -521,6 +522,7 @@ NetLabel::NetLabel( ModelPart * modelPart, ViewLayer::ViewID viewID, const ViewG
 		}
 		modelPart->setLocalProp("label", label);
 	}
+	m_label = label;
 	setInstanceTitle(label, true);
 
 	// direction is now obsolete, new netlabels use flip

src/items/symbolpaletteitem.h
@@ -96,6 +96,7 @@ protected:
 	QPointer<ConnectorItem> m_connector1;
 	bool m_voltageReference;
 	bool m_isNetLabel;
+	QString m_label;
 };
 
 

commit 23175bf0381754af548fa2f3e6a1805bc4fb4226

fix power symbols which were broken by commit  #3845, fixes #3882

src/items/symbolpaletteitem.cpp
@@ -144,7 +144,7 @@ SymbolPaletteItem::~SymbolPaletteItem() {
 void SymbolPaletteItem::removeMeFromBus(double v) {
 	foreach (ConnectorItem * connectorItem, cachedConnectorItems()) {
 		if (m_isNetLabel) {
-			LocalNetLabels.remove(m_label, connectorItem);
+			LocalNetLabels.remove(getLabel(), connectorItem);
 		}
 		else {
 			double nv = useVoltage(connectorItem);
@@ -481,6 +481,8 @@ QString SymbolPaletteItem::getLabel() {
 	if (m_voltageReference) {
 		return QString::number(m_voltage);
 	}
+	if(!m_label.isEmpty())
+		return m_label;
 	return  modelPart()->localProp("label").toString();
 }
 
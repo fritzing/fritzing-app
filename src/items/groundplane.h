de1904d16f78430834943892cf2209289904064c
Kjell Morgenstern <kjell@gmx.de>
2021-10-06 16:52:35 +0200

    Automatic clazy fixit for missing Q_OBJECT macro for Qt 6 in groundplane.h.

--- src/items/groundplane.h
+++ src/items/groundplane.h
@@ -25,6 +25,7 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 
 class GroundPlane : public PaletteItem
 {
+	Q_OBJECT
 
 public:
 	GroundPlane( ModelPart * modelPart, ViewLayer::ViewID,  const ViewGeometry &, long id, QMenu* itemMenu, bool doLabel);

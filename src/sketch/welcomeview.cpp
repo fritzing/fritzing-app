224d66f8988677d31c8fe26395bdf4e8e3920cfd
Kjell Morgenstern <kjell@gmx.de>
2021-10-08 15:56:11 +0200

    Replaced deprecated setMargin function in welcomeview.cpp.

--- src/sketch/welcomeview.cpp
+++ src/sketch/welcomeview.cpp
@@ -59,7 +59,7 @@ QString WelcomeView::m_inactiveHeaderLabelColor = "#b1b1b1";
 ///////////////////////////////////////////////////////////////////////////////
 
 void zeroMargin(QLayout * layout) {
-	layout->setMargin(0);
+	layout->setContentsMargins(0, 0, 0, 0);
 	layout->setSpacing(0);
 }
 

c07547deabbaad583939b49165a2e3c15523104c
Kjell Morgenstern <kjell@gmx.de>
2021-10-11 14:34:18 +0200

    Fixed svg unit test to use QRegExp.

--- tests/auto/test_svg/test_mfinder.cpp
+++ tests/auto/test_svg/test_mfinder.cpp
@@ -9,19 +9,22 @@
 BOOST_AUTO_TEST_CASE( mfinder )
 {
 	QString input1 = "M11488.1,3614.03L78.7354,3614.03C35.1135,3614.03,0,3578.92,0,3535.3L0,78.7298C0,35.1078,35.1135";
-	MFinder.indexIn(input1.trimmed());
-	QString priorM = MFinder.cap(1) + MFinder.cap(2) + "," + MFinder.cap(3) + " ";
+	QRegularExpressionMatch match;
+	input1.trimmed().indexOf(MFinder, 0, &match);
+	QString priorM = match.captured(1) + match.captured(2) + "," + match.captured(3) + " ";
 	BOOST_REQUIRE_EQUAL(priorM.toStdString(), std::string("M11488.1,3614.03 "));
 
 	QString input2 = "m-800.008,0 m-924.988,0 m-2550.01,0L7094.74,243.586l-133.688,0l0,-57.5969l-199.138,0l0,57.5969l-133.708,0l0,232.279z";
-	MFinder.indexIn(input2.trimmed());
-	priorM = MFinder.cap(1) + MFinder.cap(2) + "," + MFinder.cap(3) + " ";
+	QRegularExpressionMatch match2;
+	input2.trimmed().indexOf(MFinder, 0, &match2);
+	priorM = match2.captured(1) + match2.captured(2) + "," + match2.captured(3) + " ";
 	BOOST_REQUIRE_EQUAL(priorM.toStdString(), std::string("m-800.008,0 "));
 
 	// FIXME
 	// We currently ingore this kind of malformed svg path and pass on an single ", "
 	QString input3 = "m,924.988,0L3644.74,243.586l-133.688,0l0,-57.5969l-199.138,0l0,57.5969l-133.708,0l0,232.279z";
-	MFinder.indexIn(input3.trimmed());
-	priorM = MFinder.cap(1) + MFinder.cap(2) + "," + MFinder.cap(3) + " ";
+	QRegularExpressionMatch match3;
+	input3.trimmed().indexOf(MFinder, 0, &match3);
+	priorM = match3.captured(1) + match3.captured(2) + "," + match3.captured(3) + " ";
 	BOOST_REQUIRE_EQUAL(priorM.toStdString(), std::string(", "));
 }

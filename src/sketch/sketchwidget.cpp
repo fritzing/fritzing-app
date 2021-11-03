bd35ded20db0f10e33bc1cc210a80300c825e560
afaina <anfv@itu.dk>
2021-11-04 00:12:30 +0100

    when adding a wire to a bendpoint, keep the bendpoints position and move the end of the wire. Fixes #3738 #3602 and #1869

--- src/sketch/sketchwidget.cpp
+++ src/sketch/sketchwidget.cpp
@@ -3851,7 +3851,7 @@ void SketchWidget::dragWireChanged(Wire* wire, ConnectorItem * fromOnWire, Conne
 			doEmit = true;
 		}
 		if (to) {
-			extendChangeConnectionCommand(BaseCommand::CrossView, fromOnWire, to, ViewLayer::specFromID(wire->viewLayerID()), true, parentCommand);
+			extendChangeConnectionCommand(BaseCommand::CrossView, to, fromOnWire, ViewLayer::specFromID(wire->viewLayerID()), true, parentCommand);
 			doEmit = true;
 		}
 

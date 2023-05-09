f8e00f3026b2926772bea112b381550c002b1e8a
Kjell Morgenstern <kjell@gmx.de>
2023-05-09 13:36:57 +0200

    Set version to release candidate

--- src/version/version.cpp
+++ src/version/version.cpp
@@ -35,7 +35,7 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 QString Version::m_majorVersion("1");
 QString Version::m_minorVersion("0");
 QString Version::m_minorSubVersion("0");
-QString Version::m_modifier("d");
+QString Version::m_modifier("rc");
 QString Version::m_gitVersion(GIT_VERSION);
 QString Version::m_gitDate(GIT_DATE);  // want standard ISO form
 QString Version::m_date;

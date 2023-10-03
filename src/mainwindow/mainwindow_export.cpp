5c2924fe2b9420a70878a3ff93cfa87eaddb337d
Andres Faina <anfv@itu.dk>
2023-10-03 14:32:53 +0200

    if the negative terminal of a power supply is not connected and there is no ground symbol, the simulator cannot find the ground. This creates problems when requesting voltages at node 0, as ngspice cannot return the voltage at node 0. This commit fixes this by trying to find an isolated negative power supply if it has not found one before.

--- src/mainwindow/mainwindow_export.cpp
+++ src/mainwindow/mainwindow_export.cpp
@@ -1465,6 +1465,7 @@ QString MainWindow::getSpiceNetlist(QString simulationName, QList< QList<class C
 			}
 		}
 	}
+
 	// If we found no negative power supply terminals that are connected to something else, we will also check for those that are not connected.
 	if (!ground){
 		DebugDialog::debug("Netlist exporter: Trying to identify unconnected negative power supply terminals as ground");
